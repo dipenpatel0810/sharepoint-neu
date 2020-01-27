7484949f00fd7b2fea6b1ea95411dae0627f1454
Dipen Patel <patel.dip@husky.neu.edu>
2020-01-27 12:04:31 -0500

    [MOD] React scroll added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import {makeStyles, useTheme} from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
@@ -17,6 +17,16 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import {
+    Link,
+    DirectLink,
+    Element,
+    Events,
+    animateScroll as scroll,
+    scrollSpy,
+    scroller
+} from 'react-scroll'
+import Texts from './Texts';
 
 const drawerWidth = 240;
 
@@ -91,7 +101,7 @@ export default function PersistentDrawerLeft() {
 
     return (
         <div className={classes.root}>
-            <CssBaseline />
+            <CssBaseline/>
             <AppBar
                 position="fixed"
                 className={clsx(classes.appBar, {
@@ -106,7 +116,7 @@ export default function PersistentDrawerLeft() {
                         edge="start"
                         className={clsx(classes.menuButton, open && classes.hide)}
                     >
-                        <MenuIcon />
+                        <MenuIcon/>
                     </IconButton>
                     <Typography variant="h6" noWrap>
                         Persistent drawer
@@ -124,24 +134,16 @@ export default function PersistentDrawerLeft() {
             >
                 <div className={classes.drawerHeader}>
                     <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
                     </IconButton>
                 </div>
-                <Divider />
+                <Divider/>
                 <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    {['Inbox', 'Starred'].map((text, index) => (
                         <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-                <Divider />
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> :
+                                           <MailIcon/>}</ListItemIcon>
+                            <ListItemText primary={text}/>
                         </ListItem>
                     ))}
                 </List>
@@ -151,30 +153,15 @@ export default function PersistentDrawerLeft() {
                     [classes.contentShift]: open,
                 })}
             >
-                <div className={classes.drawerHeader} />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-                </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-                </Typography>
+                <div className={classes.drawerHeader}/>
+
+                <li>
+                    <Link activeClass="active" className="test1" to="test1" spy={true} smooth={true}
+                          duration={500}>Test 1</Link>
+                </li>
+
+
+                <Texts/>
             </main>
         </div>
     );
---
+++ src/components/Texts.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+
+export default function Texts(){
+    return(
+        <React.Fragment>
+            <Element name="test1" className="element" >
+                test 1
+            </Element>
+            <Typography paragraph>
+                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget
+                nulla
+                facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+                volutpat
+                consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+                risus sed
+                vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan
+                in. In
+                hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
+                aliquam sem et
+                tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
+                sollicitudin
+                nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra
+                maecenas
+                accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices
+                sagittis orci a.
+            </Typography>
+            <Typography paragraph>
+                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget
+                nulla
+                facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+                volutpat
+                consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+                risus sed
+                vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan
+                in. In
+                hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
+                aliquam sem et
+                tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
+                sollicitudin
+                nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra
+                maecenas
+                accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices
+                sagittis orci a.
+            </Typography>
+            <Typography paragraph>
+                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget
+                nulla
+                facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+                volutpat
+                consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+                risus sed
+                vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan
+                in. In
+                hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
+                aliquam sem et
+                tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
+                sollicitudin
+                nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra
+                maecenas
+                accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices
+                sagittis orci a.
+            </Typography>
+            <Typography paragraph>
+                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget
+                nulla
+                facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+                volutpat
+                consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+                risus sed
+                vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan
+                in. In
+                hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
+                aliquam sem et
+                tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
+                sollicitudin
+                nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra
+                maecenas
+                accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices
+                sagittis orci a.
+            </Typography>
+            <Typography paragraph>
+                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget
+                nulla
+                facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+                volutpat
+                consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+                risus sed
+                vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan
+                in. In
+                hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
+                aliquam sem et
+                tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
+                sollicitudin
+                nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra
+                maecenas
+                accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices
+                sagittis orci a.
+            </Typography>
+
+        </React.Fragment>
+    )
+}
